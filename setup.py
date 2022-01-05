825c873c2f7bfe621d9cbeed5b56b22657c69246
koushik global <koushikofficial@yahoo.com>
2022-01-05 12:00:39 +0000

    first commit and added raw data

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Checking quality of wafer using MLOps',
+    author='Koushik',
+    license='MIT',
+)
